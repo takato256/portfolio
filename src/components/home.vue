3787f14faa13d62cb072ce9fcc085c6b99453bf2
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-21 05:06:08 +0900

    [追加] 画像を配置

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Nakaya's Portfolio</h1>
+  <div>
+    <div class="header-container">
+      <h1 class="header-title">Nakaya's Portfolio</h1>
+    </div>
+    <img class="header-image" src="@/assets/nakaya.png">
+  </div>
 </template>
+
+<style>
+  .header-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40vh;
+  }
+
+  .header-title {
+    width: 50vw;
+    text-align: center;
+    margin-top: -20px;
+  }
+
+  .header-image {
+    max-width: 20%;
+    height: auto;
+  }
+</style>
