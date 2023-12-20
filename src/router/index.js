90154ecb97d608718d4c501943d4067543a9ad2e
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-21 00:31:00 +0900

    [追加] bootstrapをvueに導入

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
   routes: [
