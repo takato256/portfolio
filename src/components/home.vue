7ff684aafe3b111dea4b62393605326efffbba39
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-21 05:15:56 +0900

    [削除] header-container削除

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,28 +1,20 @@
 <template>
   <div>
-    <div class="header-container">
-      <h1 class="header-title">Nakaya's Portfolio</h1>
-    </div>
+    <h1 class="header-title">Nakaya's Portfolio</h1>
     <img class="header-image" src="@/assets/nakaya.png">
   </div>
 </template>
 
 <style>
-  .header-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40vh;
-  }
-
   .header-title {
     width: 50vw;
-    text-align: center;
-    margin-top: -20px;
+    margin: 0 auto;
+    margin-top: 60px;
   }
 
   .header-image {
     max-width: 20%;
     height: auto;
+    margin-top: 20px;
   }
 </style>
