5713f2470e11762d32828d096437c0ef6512a72a
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-22 02:34:43 +0900

    [変更] Productの文字を見やすいように変更

--- src/components/product.vue
+++ src/components/product.vue
@@ -1,52 +1,52 @@
 <template>
     <div>
-        <h1>Product</h1>
+        <h1>Product</h1><br>
         <div class="container-fluid bg-3 text-center">
             <div class="row">
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">ファニープレゼントアドバイザー</p>
                     <router-link to="/product/sample">
                         <img src="@/assets/funny.png" class="img-responsive" style="width:80%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">CNN可視化WEbサイト</p>
                     <img src="@/assets/cnn.png" class="img-responsive" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">HIDによるBadUSB</p>
                     <img src="@/assets/badusb.jpg" class="img-responsive" style="width:65%" alt="Image">
                 </div>
-            </div>
+            </div><br>
             <div class="row">
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">PacketStreet</p>
                     <router-link to="/product/sample">
                         <img src="@/assets/packetstreet.png" class="img-responsive" style="width:80%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">学生証在室管理Bot</p>
                     <img src="@/assets/nfc.png" class="img-responsive" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">おてがるメディカル</p>
                     <img src="@/assets/medical.png" class="img-responsive" style="width:80%" alt="Image">
                 </div>
-            </div>
+            </div><br>
             <div class="row">
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">LoRaに対話システム</p>
                     <router-link to="/product/sample">
                         <img src="@/assets/busstop.png" class="img-responsive" style="width:85%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">OpenGL太陽系</p>
                     <img src="@/assets/opengl.png" class="img-responsive" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
-                    <p>Some text..</p>
+                    <p style="font-size: 18px; margin-bottom: 5px;">NFCタグによる物品管理</p>
                     <img src="@/assets/caravan.png" class="img-responsive" style="width:90%" alt="Image">
                 </div>
             </div>
