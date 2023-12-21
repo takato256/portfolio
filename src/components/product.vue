f504e2316c3c8ab48907dcb730292e8c3cf9217e
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-21 14:20:43 +0900

    [追加] productの雛形を追加

--- src/components/product.vue
+++ src/components/product.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Product</h1>
+    <div>
+        <h1>Product</h1>
+        <div class="container-fluid bg-3 text-center">
+            <h3>Some of my Work</h3><br>
+            <div class="row">
+                <div class="col-sm-6">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+                <div class="col-sm-6">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+                <div class="col-sm-6">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+            </div>
+        </div><br>
+    </div>
 </template>
