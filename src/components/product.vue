df0c4f20ee041850244dccc8e95621176aed8193
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-22 01:22:30 +0900

    [変更] product画面を変更

--- src/components/product.vue
+++ src/components/product.vue
@@ -6,16 +6,48 @@
                 <div class="col-sm-4">
                     <p>Some text..</p>
                     <router-link to="/product/sample">
-                        <img src="@/assets/sechack.png" class="img-responsive" style="width:65%" alt="Image">
+                        <img src="@/assets/funny.png" class="img-responsive" style="width:80%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
                     <p>Some text..</p>
-                    <img src="@/assets/projectkit.png" class="img-responsive" style="width:65%" alt="Image">
+                    <img src="@/assets/cnn.png" class="img-responsive" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
                     <p>Some text..</p>
-                    <img src="@/assets/funny_certificate.png" class="img-responsive" style="width:65%" alt="Image">
+                    <img src="@/assets/badusb.jpg" class="img-responsive" style="width:65%" alt="Image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <p>Some text..</p>
+                    <router-link to="/product/sample">
+                        <img src="@/assets/packetstreet.png" class="img-responsive" style="width:80%" alt="Image">
+                    </router-link>
+                </div>
+                <div class="col-sm-4">
+                    <p>Some text..</p>
+                    <img src="@/assets/nfc.png" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+                <div class="col-sm-4">
+                    <p>Some text..</p>
+                    <img src="@/assets/medical.png" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <p>Some text..</p>
+                    <router-link to="/product/sample">
+                        <img src="@/assets/busstop.png" class="img-responsive" style="width:85%" alt="Image">
+                    </router-link>
+                </div>
+                <div class="col-sm-4">
+                    <p>Some text..</p>
+                    <img src="@/assets/opengl.png" class="img-responsive" style="width:80%" alt="Image">
+                </div>
+                <div class="col-sm-4">
+                    <p>Some text..</p>
+                    <img src="@/assets/caravan.png" class="img-responsive" style="width:90%" alt="Image">
                 </div>
             </div>
         </div><br>
