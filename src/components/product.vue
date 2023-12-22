520aa09a2da8611f03c7572ac80a9dca4cc3e9c3
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-23 00:09:10 +0900

    [追加] 画像の縁取りを追加

--- src/components/product.vue
+++ src/components/product.vue
@@ -6,50 +6,58 @@
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">ファニープレゼントアドバイザー</p>
                     <router-link to="/product/sample">
-                        <img src="@/assets/funny.png" class="img-responsive" style="width:80%" alt="Image">
+                        <img src="@/assets/funny.png" class="img-responsive img-bordered" style="width:80%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">CNN可視化WEBサイト</p>
-                    <img src="@/assets/cnn.png" class="img-responsive" style="width:80%" alt="Image">
+                    <img src="@/assets/cnn.png" class="img-responsive img-bordered" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">HIDによるBadUSB</p>
-                    <img src="@/assets/badusb.jpg" class="img-responsive" style="width:65%" alt="Image">
+                    <img src="@/assets/badusb.jpg" class="img-responsive img-bordered" style="width:65%" alt="Image">
                 </div>
             </div><br>
             <div class="row">
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">~ネットワーク可視化ツール~ Packet Street</p>
                     <router-link to="/product/sample">
-                        <img src="@/assets/packetstreet.png" class="img-responsive" style="width:80%" alt="Image">
+                        <img src="@/assets/packetstreet.png" class="img-responsive img-bordered" style="width:80%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">学生証在室管理Bot</p>
-                    <img src="@/assets/nfc.png" class="img-responsive" style="width:80%" alt="Image">
+                    <img src="@/assets/nfc.png" class="img-responsive img-bordered" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">おてがるメディカル</p>
-                    <img src="@/assets/medical.png" class="img-responsive" style="width:80%" alt="Image">
+                    <img src="@/assets/medical.png" class="img-responsive img-bordered" style="width:80%" alt="Image">
                 </div>
             </div><br>
             <div class="row">
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">LoRaによる対話システム</p>
                     <router-link to="/product/sample">
-                        <img src="@/assets/busstop.png" class="img-responsive" style="width:85%" alt="Image">
+                        <img src="@/assets/busstop.png" class="img-responsive img-bordered" style="width:85%" alt="Image">
                     </router-link>
                 </div>
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">OpenGL太陽系</p>
-                    <img src="@/assets/opengl.png" class="img-responsive" style="width:80%" alt="Image">
+                    <img src="@/assets/opengl.png" class="img-responsive img-bordered" style="width:80%" alt="Image">
                 </div>
                 <div class="col-sm-4">
                     <p style="font-size: 18px; margin-bottom: 5px;">NFCタグによる物品管理</p>
-                    <img src="@/assets/caravan.png" class="img-responsive" style="width:90%" alt="Image">
+                    <img src="@/assets/caravan.png" class="img-responsive img-bordered" style="width:90%" alt="Image">
                 </div>
             </div>
         </div><br>
     </div>
 </template>
+
+<style>
+  .img-bordered {
+    border: 2px solid #ddd; /* 枠線のスタイルを設定 */
+    border-radius: 5px; /* 角の丸みを設定 */
+    padding: 5px; /* 枠線と画像の間隔を設定 */
+  }
+</style>
