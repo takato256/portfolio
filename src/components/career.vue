884b84f2236ef1f1b643fac4a0efbb25ba3790a4
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-23 20:36:10 +0900

    [追加] career情報を追加

--- src/components/career.vue
+++ src/components/career.vue
@@ -2,17 +2,14 @@
     <div>
         <h1>Career</h1><br>
         <div v-for="(card, index) in cards" :key="index" class="card-container">
+            <div class="year" v-if="card.year">{{ card.year }}</div>
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">{{ card.title }}</h4>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ card.subtitle }}</h6>
+                    <h3 class="card-title">{{ card.title }}</h3>
                     <p class="card-text">{{ card.content }}</p>
-                    <a href="#!" class="card-link">Card link</a>
-                    <a href="#!" class="card-link">Another link</a>
                 </div>
             </div>
             <div class="line" v-if="index < cards.length - 1"></div>
-            <div class="year" v-if="card.year">{{ card.year }}</div>
         </div>
     </div>
 </template>
@@ -22,8 +19,15 @@ export default {
   data () {
     return {
       cards: [
-        { title: 'Card title 1', subtitle: 'Card subtitle 1', content: 'Content for card 1', year: '2022' },
-        { title: 'Card title 2', subtitle: 'Card subtitle 2', content: 'Content for card 2', year: '2023' }
+        { title: '滑川高等学校', content: '普通科 卒業', year: '2021/3' },
+        { title: '金沢工業大学', content: '工学部 情報工学科 入学', year: '2021/4' },
+        { title: 'FUJITSU Academy', content: '2days event', year: '2021/10' },
+        { title: 'SecHack365', content: '開発駆動コース 仲山ゼミ', year: '2022/5' },
+        { title: 'セキュリティ・ミニキャンプ', content: 'in 東京 2022', year: '2022/12' },
+        { title: '技育CAMPキャラバン in 金沢', content: 'CARTA HOLDINGS賞 受賞', year: '2023/7' },
+        { title: 'KLab Expert Camp', content: 'TCP/IPプロトコルスタック自作', year: '2023/8' },
+        { title: '技育展2023 決勝大会', content: 'ゆめみ賞 受賞', year: '2023/9' },
+        { title: 'NTT東日本 インターンシップ', content: 'ネットワークエンジニア仕事体験コース', year: '2023/12' }
       ]
     }
   }
@@ -37,9 +41,13 @@ export default {
     align-items: center;
 }
 
+.card-body {
+  padding: 50px;
+}
+
 .line {
-    width: 2px;
-    height: 50px;
+    width: 4px;
+    height: 30px;
     background-color: #000;
     margin: 10px 0;
 }
