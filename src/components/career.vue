6d179bab2599d05e06870a82ce3f12105e775c6a
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-24 02:59:25 +0900

    [変更] 文字が都合の良いところで改行されるように変更

--- src/components/career.vue
+++ src/components/career.vue
@@ -11,8 +11,8 @@
             </div>
             <div class="col-lg-6 d-flex align-items-center">
               <div class="card-body">
-                <h4 class="card-title">{{ card.title }}</h4>
-                <p class="card-text">{{ card.content }}</p>
+                <h4 class="card-title" style="white-space: pre-wrap;">{{ card.title }}</h4>
+                <p class="card-text" style="white-space: pre-wrap;">{{ card.content }}</p>
               </div>
             </div>
           </div>
@@ -31,11 +31,11 @@ export default {
         { title: '金沢工業大学', content: '工学部 情報工学科 入学', image: require('@/assets/kit.jpeg'), year: '2021/4' },
         { title: 'FUJITSU Academy', content: '2days event', image: require('@/assets/nakaya.png'), year: '2021/10' },
         { title: 'SecHack365', content: '開発駆動コース 仲山ゼミ', image: require('@/assets/SecHack365.png'), year: '2022/5' },
-        { title: 'セキュリティ・ミニキャンプ', content: 'in 東京 2022', image: require('@/assets/seccamp.jpg'), year: '2022/12' },
-        { title: '技育CAMPキャラバン in 金沢', content: 'CARTA HOLDINGS賞 受賞', image: require('@/assets/caravan.jpg'), year: '2023/7' },
+        { title: 'セキュリティ\nミニキャンプ', content: 'in 東京 2022', image: require('@/assets/seccamp.jpg'), year: '2022/12' },
+        { title: '技育CAMPキャラバン\nin 金沢', content: 'CARTA HOLDINGS賞 受賞', image: require('@/assets/caravan.jpg'), year: '2023/7' },
         { title: 'KLab Expert Camp', content: 'TCP/IPプロトコルスタック自作', image: require('@/assets/klab.jpg'), year: '2023/8' },
         { title: '技育展2023 決勝大会', content: 'ゆめみ賞 受賞', image: require('@/assets/geekten.jpg'), year: '2023/9' },
-        { title: 'NTT東日本 インターンシップ', content: 'ネットワークエンジニア仕事体験コース', image: require('@/assets/nakaya.png'), year: '2023/12' }
+        { title: 'NTT東日本\nインターンシップ', content: 'ネットワークエンジニア\n仕事体験コース', image: require('@/assets/nakaya.png'), year: '2023/12' }
       ]
     }
   }
